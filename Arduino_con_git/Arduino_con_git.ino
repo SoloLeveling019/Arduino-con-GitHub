4765d6858193c2da35cf276c5d0aa37b49c72d8e
DanielSosa25 <1332022043@alumnos.unp.edu.pe>
2025-05-23 11:16:19 -0500

    Primer1
    LEd

--- Arduino_con_git/Arduino_con_git.ino
+++ Arduino_con_git/Arduino_con_git.ino
@@ -1,11 +1,19 @@
 int led = 9;
-int brillo = 128;
+int brillo = 0;  // Comenzamos apagado
+bool estado = false; // Estado del LED, false = apagado, true = encendido
 
 void setup() {
   pinMode(led, OUTPUT);
 }
 
 void loop() {
+  if (estado) {
+    brillo = 255;  // Encender LED al máximo brillo
+  } else {
+    brillo = 0;    // Apagar LED
+  }
+
   analogWrite(led, brillo);
-  delay(1000);
+  estado = !estado;  // Cambiar estado para el próximo ciclo
+  delay(1000);       // Esperar 1 segundo
 }
